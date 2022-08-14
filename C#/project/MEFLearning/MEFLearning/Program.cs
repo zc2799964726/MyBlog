a51f9322c2b1056231c4dded39c6bec0c6be4ed9
zc2799964726 <zc2799964726@163.com>
2022-08-14 22:08:52 +0800

    新建MEF学习1

---
+++ C#/project/MEFLearning/MEFLearning/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+using System.ComponentModel.Composition.Hosting;
+
+namespace MEFLearning
+{
+    class Program
+    {
+        //
+        [Export("HistoryBook", typeof(IBookService))]
+        //[Import("HistoryBook")]
+        public IBookService Service { get; set; }
+
+        static void Main(string[] args)
+        {
+            Program pro = new Program();
+            pro.Compose();
+            if (pro.Service != null)
+            {
+                Console.WriteLine(pro.Service.GetBookName());
+            }
+            Console.Read();
+        }
+
+        private void Compose()
+        {
+            var catalog = new AssemblyCatalog(Assembly.GetExecutingAssembly());
+            CompositionContainer container = new CompositionContainer(catalog);
+            container.ComposeParts(this);
+        }
+    }
+}
